e6c91868c0a121337203390efd3f3f988ca4eff5
EGOIST <0x142857@gmail.com>
2016-07-05 23:18:05 +0800

    open devtools in dev mode only

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const menubar = require('menubar')
 const {globalShortcut, ipcMain} = require('electron')
 
+const isDev = process.env.NODE_ENV === 'development'
+
 const mb = menubar({
   dir: process.cwd(),
   wdith: 360,
@@ -13,7 +15,7 @@ mb.on('ready', () => {
 })
 
 mb.on('after-create-window', () => {
-  mb.window.openDevTools()
+  if (isDev) mb.window.openDevTools()
   // refresh webview only
   globalShortcut.register('CommandOrControl+R', () => {
     mb.window.webContents.send('refresh')
